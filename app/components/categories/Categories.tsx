47bd5ca8d6c164db9a11a2dcaedbc358e711e954
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-07-08 13:03:47 +0600

    hotel type and location input

--- app/components/categories/Categories.tsx
+++ app/components/categories/Categories.tsx
@@ -4,48 +4,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 
 import Container from "../Container";
 import CategoryBox from "./CategoryBox";
-import { AiFillBank } from "react-icons/ai";
-import { FaUmbrellaBeach } from "react-icons/fa";
-import { GiHills } from "react-icons/gi";
-import { BiHomeHeart } from "react-icons/bi";
-
-const categories = [
-  {
-    label: "Pools",
-    description: "This is property has a beautiful pool!",
-    icon: AiFillBank,
-  },
-  {
-    label: "Islands",
-    description: "This property is on an island!",
-    icon: AiFillBank,
-  },
-  {
-    label: "Lake",
-    description: "This property is near a lake!",
-    icon: FaUmbrellaBeach,
-  },
-  {
-    label: "Skiing",
-    description: "This property has skiing activies!",
-    icon: FaUmbrellaBeach,
-  },
-  {
-    label: "Modern",
-    description: "This property has skiing activies!",
-    icon: GiHills,
-  },
-  {
-    label: "Hotel",
-    description: "This property has skiing activies!",
-    icon: BiHomeHeart,
-  },
-  {
-    label: "Resort",
-    description: "This property has skiing activies!",
-    icon: FaUmbrellaBeach,
-  },
-];
+import { categories } from "@/app/data/categories";
 
 const Categories = () => {
   const params = useSearchParams();
---
+++ app/data/categories.ts
@@ -0,0 +1,42 @@
+import { AiFillBank } from "react-icons/ai";
+import { FaUmbrellaBeach } from "react-icons/fa";
+import { GiHills } from "react-icons/gi";
+import { BiHomeHeart } from "react-icons/bi";
+
+export const categories = [
+  {
+    label: "Hotel",
+    description: "This is property has a beautiful pool!",
+    icon: AiFillBank,
+  },
+  {
+    label: "Modern",
+    description: "This property is on an island!",
+    icon: AiFillBank,
+  },
+  {
+    label: "Resort",
+    description: "This property is near a lake!",
+    icon: FaUmbrellaBeach,
+  },
+  {
+    label: "Beach",
+    description: "This property has skiing activies!",
+    icon: FaUmbrellaBeach,
+  },
+  {
+    label: "Hill",
+    description: "This property has skiing activies!",
+    icon: GiHills,
+  },
+  {
+    label: "Villa",
+    description: "This property has skiing activies!",
+    icon: BiHomeHeart,
+  },
+  {
+    label: "Apartment Hotel",
+    description: "This property has skiing activies!",
+    icon: FaUmbrellaBeach,
+  },
+];
