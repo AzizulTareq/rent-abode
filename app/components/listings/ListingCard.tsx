5c079d211fdbea2066a4703b30d2de8172d8f7b6
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-07-17 21:36:52 +0600

    listing all abodes in home page

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -75,7 +75,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
       onClick={() => router.push(`/listings/${data.id}`)}
       className="col-span-1 cursor-pointer group"
     >
-      <div className="flex flex-col gap-2 w-full">
+      <div className="flex flex-col gap-2 w-full pb-16">
         <div
           className="
             aspect-square
