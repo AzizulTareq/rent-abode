20444fdf172ee494f1a7706b6fce2c1fdaa0aa72
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-07-26 23:26:31 +0600

    another step

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -148,6 +148,39 @@ const SearchModal = () => {
     )
   }
 
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="More information"
+          subtitle="Find your perfect place!"
+        />
+        <Counter
+          onChange={(value) => setGuestCount(value)}
+          value={guestCount}
+          title="Guests"
+          subtitle="How many guests are coming?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setRoomCount(value)}
+          value={roomCount}
+          title="Rooms"
+          subtitle="How many rooms do you need?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => {
+            setBathroomCount(value)
+          }}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bahtrooms do you need?"
+        />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={searchModal.isOpen}
